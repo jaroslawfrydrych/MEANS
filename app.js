ff6bf93e6ebe40613c1c4886043358910def6946
Jarosław Frydrych <jarek.frydrych@gmail.com>
2018-04-06 10:48:18 +0200

    node server

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 require("babel-register");
 const app = require('./server');
 const http = require('http');
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 app.set('port', port);
 const server = http.createServer(app);
 
@@ -23,5 +23,25 @@ function onListening() {
 }
 
 function onError(error) {
-    console.warn(error);
+    if (error.syscall !== "listen") {
+        throw error;
+    }
+
+    var bind = typeof port === "string"
+        ? "Pipe " + port
+        : "Port " + port;
+
+    // handle specific listen errors with friendly messages
+    switch (error.code) {
+        case "EACCES":
+            console.error(bind + " requires elevated privileges");
+            process.exit(1);
+            break;
+        case "EADDRINUSE":
+            console.error(bind + " is already in use");
+            process.exit(1);
+            break;
+        default:
+            throw error;
+    }
 }
