1dfe2c5434161461b413450e3b41d16afc38bbd5
Jarosław Frydrych <jarek.frydrych@gmail.com>
2018-04-06 21:43:27 +0200

    config handler

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,11 +1,14 @@
 "use strict";
+var APP_CONFIG = require("../../config/config.json");
 var Route;
 (function (Route) {
     var Index = (function () {
         function Index() {
         }
         Index.prototype.index = function (req, res, next) {
-            res.render('index');
+            res.render('index', {
+                title: APP_CONFIG.title ? APP_CONFIG.title : ''
+            });
         };
         return Index;
     }());
--- backend/routes/index.ts
+++ backend/routes/index.ts
@@ -1,9 +1,12 @@
 import * as express from 'express';
+import * as APP_CONFIG from '../../config/config.json';
 
 module Route {
     export class Index {
         public index(req: express.Request, res: express.Response, next: express.NextFunction) {
-            res.render('index')
+            res.render('index', {
+                title: APP_CONFIG.title ? APP_CONFIG.title : ''
+            })
         }
     }
 }
