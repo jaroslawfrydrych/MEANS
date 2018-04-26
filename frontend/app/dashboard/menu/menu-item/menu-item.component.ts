52afdcfeef4fd93b925b630f8822d021cfe3e866
Jarosław Frydrych <jarek.frydrych@gmail.com>
2018-04-26 17:00:36 +0200

    menu

--- frontend/app/dashboard/menu/menu-item/menu-item.component.ts
+++ frontend/app/dashboard/menu/menu-item/menu-item.component.ts
@@ -1,20 +1,31 @@
-import {Component, ContentChildren, ElementRef, Input, OnInit, QueryList, ViewChild} from '@angular/core';
+import {AfterContentInit, Component, ContentChild, ContentChildren, ElementRef, Input, OnInit, QueryList, ViewChild} from '@angular/core';
 
 @Component({
     selector: 'app-menu-item',
     templateUrl: './menu-item.component.html',
     styleUrls: ['./menu-item.component.scss']
 })
-export class MenuItemComponent implements OnInit {
+export class MenuItemComponent implements OnInit, AfterContentInit {
 
     @Input() public path: String = '';
+    @Input() public label: String = '';
     @ViewChild('menuItemContent') menuItemContent: ElementRef;
-    @ContentChildren(MenuItemComponent) subMenuItems: QueryList<MenuItemComponent>;
+    @ViewChild('subMenuItemsContent') subMenuItemsContent: ElementRef;
+    @ContentChildren(MenuItemComponent) menuItemsContent: QueryList<MenuItemComponent>;
+    public menuItems: Array<MenuItemComponent>;
 
     constructor() {
     }
 
     ngOnInit() {
+    }
+
+    ngAfterContentInit() {
+        this.menuItems = this.menuItemsContent.filter((item, index) => {
+            return index > 0;
+        });
+
+        console.log(this.menuItems);
     }
 
 }
