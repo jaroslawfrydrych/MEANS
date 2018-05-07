b37482896ac36fe6f8f973119ef57a13e4b0fbe5
Jarosław Frydrych <jarek.frydrych@gmail.com>
2018-05-07 23:49:49 +0200

    cleanup

--- projects/testlib/src/public_api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of testlib
- */
-
-export * from './lib/testlib.service';
-export * from './lib/testlib.component';
-export * from './lib/testlib.module';
--- proxy.conf.js
+++ proxy.conf.js
@@ -10,12 +10,16 @@ try {
     console.log('To set different proxy target crate proxy.config.json, with key "target", and specified domain and port of your api.')
 }
 
+const target = PROXY_JSON.target || 'http://localhost:' + CONFIG.port;
+
 const PROXY_CONFIG = {
     '/api': {
-        target: PROXY_JSON.target || 'http://localhost:' + CONFIG.port,
+        target,
         secure: false,
         changeOrigin: true
     }
 };
+
+console.log('Proxy is pointing at ' + target);
 
 module.exports = PROXY_CONFIG;
