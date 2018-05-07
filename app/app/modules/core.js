351b0f6dea13139bb1bbec6c12dd81e4d1923d86
Jarosław Frydrych <jarek.frydrych@gmail.com>
2018-05-07 14:43:24 +0200

    rxjs fix

--- app/app/modules/core.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var core_controller_1 = require("./core/core.controller");
-exports.asdHandler = core_controller_1.asdHandler;
--- app/app/modules/core/core.controller.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function asdHandler(req, res, next) {
-    res.send(req.auth);
-}
-exports.asdHandler = asdHandler;
